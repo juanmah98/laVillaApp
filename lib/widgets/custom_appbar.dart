a607d9f00144234502e5f17f2707279080c3d59f
JuanmaH198 <juanmah1998@gmail.com>
2024-01-23 21:03:13 +0100

    google maps

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -6,6 +6,28 @@ class CustomAppBar extends StatelessWidget{
   const CustomAppBar({super.key});
 
 
+  void displayCard(BuildContext context){
+    showDialog(
+      barrierDismissible: true,
+    context: context, 
+    builder: (context){
+      return const AlertDialog(
+        elevation: 5,
+        title: Text("The Orange House"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Contenido de la villa"),
+            Text("Contenido de la villa"),
+            Text("Contenido de la villa"),
+            Text("Contenido de la villa"),
+            Text("Contenido de la villa"),
+            Text("Contenido de la villa")
+          ]),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
    
@@ -80,21 +102,25 @@ class CustomAppBar extends StatelessWidget{
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                child: Container(
-                  width: double.infinity,
-                    height: 90,
-                    decoration: BoxDecoration(
-                      color: Colors.blueGrey,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 2),
-                        ),
-                      ],
-                    ),
+                child: InkWell(
+                  onTap: () => displayCard(context),
+                  child: const Card(
+                   elevation: 30,
+                   child: Column(
+                    children: [
+                      ClipRRect(
+                         borderRadius: BorderRadius.all(Radius.circular(25)),
+                        child: Image(
+                          
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                          height: 100,
+                          image: NetworkImage('https://img.freepik.com/fotos-premium/hermosa-casa-naranja-exterior-increible-vista-frente-casa_884986-88.jpg')),
+                      )
+                    ],
+                   ),
+                      
+                  ),
                 ),
               ),
               Padding(
@@ -104,7 +130,7 @@ class CustomAppBar extends StatelessWidget{
                     height: 120,
                     decoration: BoxDecoration(
                       color: Colors.grey,
-                      borderRadius: BorderRadius.circular(30),
+                      borderRadius: BorderRadius.circular(25),
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black.withOpacity(0.5),
